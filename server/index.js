969e7139cce089dbac29f47250315efbf900cd3f
Ryan De Leon <ryandeleon0@gmail.com>
2022-04-25 13:33:20 -0700

    helpful and reported put requests done, add review post request done

--- server/index.js
+++ server/index.js
@@ -38,15 +38,8 @@ app.get('/reviews', (req, res, next) => {
   FROM photos p
   WHERE p.review_id = r.id) as photos
    FROM reviews r
-  WHERE r.product_id = 2;`
-
-
-//   const information = {
-//     'product': product_id,
-//     'page': page,
-//     'count': count,
-//     'results': []
-// }
+  WHERE r.product_id = ${product_id}
+  LIMIT ${count};`
 
   pool
     .query(reviewQuery)
@@ -75,33 +68,12 @@ app.get('/reviews/meta', (req, res, next) => {
   const queryTest1 = `SELECT rating, recommend, photo FROM reviews JOIN photos WHERE product_id = ${product_id} && review.id = photos.id; `
   // query
 
-  const queryMeta = `SELECT json_object_agg(name, testing)  as characteristics
-  FROM
-  (SELECT c.name, json_build_object(
-    'id', cr.id,
-    'value', cr.value
-  ) as testing
-  FROM characteristics c
-  JOIN characteristic_reviews cr
-  ON c.id = cr.characteristic_id
-  WHERE cr.review_id = 1
-  LIMIT 5) as test;`
-
-  const ratingsQuery = `with table1 as(
-    SELECT r.rating,COUNT(*) as count
-    FROM reviews r
-    WHERE r.product_id = 2
-    GROUP BY r.rating)
-
-    SELECT json_object_agg(table1.rating, table1.count) as ratings
-    from table1;`
-
-  const TEST = `with table1 as (
+  const reviewsMetaQuery = `with table1 as (
     SELECT table1.product_id, json_object_agg((CAST(table1.rating AS integer)), table1.count) as ratings
     FROM (
       SELECT r.product_id, r.rating,COUNT(*) as count
       FROM reviews r
-      WHERE r.product_id = 2
+      WHERE r.product_id = 1
       GROUP BY r.product_id, r.rating) as table1
     GROUP BY table1.product_id
   ),
@@ -110,7 +82,7 @@ app.get('/reviews/meta', (req, res, next) => {
     FROM (
       SELECT  r.product_id, r.recommend,COUNT(*) as count
       FROM reviews r
-      WHERE r.product_id = 2
+      WHERE r.product_id = 1
       GROUP BY r.product_id, r.recommend) as table2
     GROUP BY table2.product_id
   ),
@@ -122,45 +94,68 @@ app.get('/reviews/meta', (req, res, next) => {
     FROM characteristics c
     JOIN characteristic_reviews cr
     ON c.id = cr.characteristic_id
-    WHERE c.product_id = 2
+    WHERE c.product_id = 1
     GROUP BY c.product_id, cr.review_id
     LIMIT 1)
 
-  SELECT table1.ratings, table2.recommend, table3.characteristics
+  SELECT table1.product_id, table1.ratings, table2.recommend, table3.characteristics
   FROM table1
   JOIN table2
   ON table1.product_id = table2.product_id
   JOIN  table3 ON table1.product_id = table3.product_id;`
 
   pool
-    .query(TEST)
+    .query(reviewsMetaQuery)
     .then(data => {
-      // console.log(JSON.parse(JSON.stringify(data.rows)))
       res.send(data.rows)
     })
     .catch(err => console.log(err));
 })
 
-app.post('/reviews/', (req, res, next) => {
-  let product_id = req.body.product_id;
-  console.log(req.body, 'this is params')
+app.post('/reviews', (req, res, next) => {
+  let product_id = req.query.product_id;
+  let rating = req.query.rating;
+  let summary = req.query.summary;
+  let body = req.query.body;
+  let recommend = req.query.recommend
+  let name = req.query.name;
+  let email = req.query.email;
+  // let timestamp = SELECT LOCALTIMESTAMP(0);
+
+
+  console.log(req.query, 'this is reqbody')
   // INSERT IN TO TABLE
 
+  const insertToTable = `
+    INSERT INTO reviews
+    (product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email, reported, helpfulness)
+    VALUES
+    (${product_id}, ${rating}, EXTRACT(EPOCH from CURRENT_TIMESTAMP)::bigint * 1000, '${summary}', '${body}', ${recommend}, '${name}', '${email}', false, 0)
+    RETURNING rating;`;
+
+
+
   pool
-    .query()
+    .query(insertToTable)
     .then(data => {
-      // res.send(data.rows)
-      console.log('sucessfully posted')
+      console.log(data)
+      res.send('sucessfully posted')
     })
     .catch(err => console.log(err));
 })
 
 app.put('/reviews/:review_id/helpful', (req, res, next) => {
-  console.log(req.body, 'this is params')
-  // UPDATE helpfulness in table for product_id
+
+  console.log(req, 'this is query')
+  let review_id = req.params.review_id;
+
+  const updateHelp = `UPDATE reviews
+  SET helpfulness = helpfulness::int + 1
+  WHERE reviews.id = ${review_id}
+  RETURNING id;`
 
   pool
-    .query()
+    .query(updateHelp)
     .then(data => {
       console.log('helpful')
       res.send(data.rows)
@@ -169,14 +164,18 @@ app.put('/reviews/:review_id/helpful', (req, res, next) => {
 })
 
 app.put('/reviews/:review_id/report', (req, res, next) => {
-  console.log(req.body, 'this is params')
-  // UPDATE reported # in table
+  console.log(req, 'this is params')
+  let review_id = req.params.review_id
+  const report = `UPDATE reviews
+  SET reported = True
+  WHERE reviews.id = ${review_id}
+  RETURNING id;`
 
   pool
-    .query()
+    .query(report)
     .then(data => {
       console.log('successfully posted')
-      // res.send(data.rows)
+      res.send('successfully reported!')
     })
     .catch(err => console.log(err));
 })
